ea20a8fc45566628169003732fb81268f6c16882
Sonja Blesch <sonja.blesch@vodafone.de>
2024-10-01 10:04:43 +0200

    Initial commit

---
+++ demo_script.R
@@ -0,0 +1,22 @@
+setwd("/Users/sonja/uni/semester 5 nhh/R/lectures/git and github/git_lesson")
+
+
+#Headline
+
+
+x <- rnorm(500)
+
+
+y <- rnorm(500)
+
+
+#Important feature
+
+#New feature
+
+#normal development
+
+#Important feature
+
+#an alternative way forward
+
